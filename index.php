df24752b3ba32547c035bf292f5053e5eda4e0ea
lisafelic <lisafelicia18@gmail.com>
2023-07-24 15:17:57 +0700

    adding index php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+$conn= mysqli_connect("testsql","root","mantap@18","mantap");
+
+if ($conn->connect_error){
+	die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected to mySQL Succesfully";
+
+//query to database
+$sql = "select name from account where id = '1' ";
+$result = $conn->query($sql);
+
+//print to web
+$row = $result ->fetch_assoc();
+	echo "<br>Hello, " . $row["name"] . "<br>Have a nice day folks!" ;
+
+?>
